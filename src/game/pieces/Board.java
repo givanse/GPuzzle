6e383e022561327cb7c002f13828c175d93cf791
givanse <silvag777@gmail.com>
2013-07-22 10:57:56 -0500

    generate a random pair of falling squares

--- src/game/pieces/Board.java
+++ src/game/pieces/Board.java
@@ -1,5 +1,9 @@
 package game.pieces;
 
+import game.pieces.Square.SquareType;
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
  *
  * @author givanse
@@ -8,8 +12,9 @@ public class Board {
     
     private final int width;
     private final int height;
-    private final boolean coordinates[][];
+    private final boolean availabilityMatrix[][];
     private final Square squares[][];
+    private final ArrayList<Square> fallingSquares = new ArrayList<Square>(2);
     
     public enum SwapDirection {LEFT, RIGHT, UP, DOWN};
     public static int WIDTH_IN_SQUARES = 12;  // number of squares 
@@ -19,22 +24,36 @@ public class Board {
         this(Board.WIDTH_IN_SQUARES, Board.HEIGHT_IN_SQUARES);
     }
     
+    public Board(Square[][] squares) {
+        this(squares[0].length, squares.length); 
+         for(int x = 0; x < this.width ; x++)
+            for(int y = 0; y < this.height; y++) {
+                /* Update all the positions of the availabilityMatrix. */
+                this.updatePosition(x, y, this.isPositionAvailable(x, y));
+                this.squares[x][y] = squares[x][y];
+            }
+    }
+    
     public Board(int widthInSquares, int heightInSquares) {
         this.width = widthInSquares;
         this.height = heightInSquares;
-        this.coordinates = new boolean[this.width][this.height];
+        this.availabilityMatrix = new boolean[this.width][this.height];
         this.squares = new Square[this.width][this.height];
+        this.generateRandomFallingPair();
     }
     
-    public Board(Square[][] squares) {
-        this.squares = squares;
-        this.width = this.squares[0].length;
-        this.height = this.squares.length;
-        this.coordinates = new boolean[this.width][this.height];
-         for(int x = 0; x < this.width ; x++)
-            for(int y = 0; y < this.height; y++) {
-                this.updatePosition(x, y, this.isPositionAvailable(x, y));
-            }
+    private void generateRandomFallingPair() {
+        Random random = new Random(System.currentTimeMillis());
+        int randomX = random.nextInt(this.width);
+        int constantY = 0;
+        SquareType sqrTypes[] = SquareType.values();
+        int randomType = random.nextInt(sqrTypes.length);
+        Square square1 = new Square(randomX, constantY, sqrTypes[randomType]);
+        this.fallingSquares.add(square1);
+        randomX = (randomX == this.width-1) ? randomX - 1 : randomX + 1;
+        randomType = random.nextInt(sqrTypes.length);
+        Square square2 = new Square(randomX, constantY, sqrTypes[randomType]);
+        this.fallingSquares.add(square2);
     }
     
     /* Public methods */
@@ -47,12 +66,16 @@ public class Board {
         return this.squares;
     }
     
-    public boolean isPositionAvailable(int x, int y) {
+    public ArrayList<Square> getFallingSquares() {
+        return this.fallingSquares;
+    }
+    
+    public final boolean isPositionAvailable(int x, int y) {
         return false;
     }
     
-    public void updatePosition(int x, int y, boolean isAvailable) {
-        this.coordinates[x][y] =  isAvailable;
+    public final void updatePosition(int x, int y, boolean isAvailable) {
+        this.availabilityMatrix[x][y] =  isAvailable;
     }
     
     public boolean isValidSwap(int x, int y, SwapDirection dir) {
