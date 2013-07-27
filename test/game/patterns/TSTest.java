9dce10ad06d6870486f74b2664ad19734aeca661
givanse <silvag777@gmail.com>
2013-07-26 22:26:06 -0500

    remove 'message'

--- test/game/patterns/TSTest.java
+++ test/game/patterns/TSTest.java
@@ -25,46 +25,37 @@ public class TSTest {
                 .insertSquare(1, 2, Square.SquareColour.BLUE)
                 .insertSquare(2, 2, Square.SquareColour.BLUE);
         /* pattern match a S */
-        String message;
         int expecteds[][] = new int[0][0];
         int actuals[][];
         int x, y;
         for(x = 0; x < squares.getWidth(); x++) {
             y = 0;
             actuals = TetrominoType.S.isPatternFound(x, y, squares);
-            message = "S (" + x + ", " + y + ")";
-            assertArrayEquals(message, expecteds, actuals);
+            assertArrayEquals(expecteds, actuals);
             y = 3;
             actuals = TetrominoType.S.isPatternFound(x, y, squares);
-            message = "S (" + x + ", " + y + ")";
-            assertArrayEquals(message, expecteds, actuals);
+            assertArrayEquals(expecteds, actuals);
         }
         
         x = 0; y = 1;
         actuals = TetrominoType.S.isPatternFound(x, y, squares);
-        message = "S (" + x + ", " + y + ")";
-        assertArrayEquals(message, expecteds, actuals);
+        assertArrayEquals(expecteds, actuals);
         x = 0; y = 2;
         actuals = TetrominoType.S.isPatternFound(x, y, squares);
-        message = "S (" + x + ", " + y + ")";
-        assertArrayEquals(message, expecteds, actuals);
+        assertArrayEquals(expecteds, actuals);
         x = 1; y = 1;
         actuals = TetrominoType.S.isPatternFound(x, y, squares);
-        message = "S (" + x + ", " + y + ")";
-        assertArrayEquals(message, expecteds, actuals);
+        assertArrayEquals(expecteds, actuals);
         
         x = 3; y = 2;
         actuals = TetrominoType.S.isPatternFound(x, y, squares);
-        message = "S (" + x + ", " + y + ")";
-        assertArrayEquals(message, expecteds, actuals);
+        assertArrayEquals(expecteds, actuals);
         x = 4; y = 1;
         actuals = TetrominoType.S.isPatternFound(x, y, squares);
-        message = "S (" + x + ", " + y + ")";
-        assertArrayEquals(message, expecteds, actuals);
+        assertArrayEquals(expecteds, actuals);
         x = 4; y = 2;
         actuals = TetrominoType.S.isPatternFound(x, y, squares);
-        message = "S (" + x + ", " + y + ")";
-        assertArrayEquals(message, expecteds, actuals);
+        assertArrayEquals(expecteds, actuals);
         
         /**
          * 01234
@@ -75,26 +66,22 @@ public class TSTest {
         x = 2; y = 1;
         expecteds = new int[][]{{2, 1}, {3, 1}, {2, 2}, {1, 2}};
         actuals = TetrominoType.S.isPatternFound(x, y, squares);
-        message = "S (" + x + ", " + y + ")";
-        assertArrayEquals(message, expecteds, actuals);
+        assertArrayEquals(expecteds, actuals);
         
         x = 3; y = 1;
         expecteds = new int[][]{{3, 1}, {2, 1}, {2, 2}, {1, 2}};
         actuals = TetrominoType.S.isPatternFound(x, y, squares);
-        message = "S (" + x + ", " + y + ")";
-        assertArrayEquals(message, expecteds, actuals);
+        assertArrayEquals(expecteds, actuals);
         
         x = 2; y = 2;
         expecteds = new int[][]{{2, 2}, {1, 2}, {2, 1}, {3, 1}};
         actuals = TetrominoType.S.isPatternFound(x, y, squares);
-        message = "S (" + x + ", " + y + ")";
-        assertArrayEquals(message, expecteds, actuals);
+        assertArrayEquals(expecteds, actuals);
         
         x = 1; y = 2;
         expecteds = new int[][]{{1, 2}, {2, 2}, {2, 1}, {3, 1}};
         actuals = TetrominoType.S.isPatternFound(x, y, squares);
-        message = "S (" + x + ", " + y + ")";
-        assertArrayEquals(message, expecteds, actuals);
+        assertArrayEquals(expecteds, actuals);
     }
     
 }
