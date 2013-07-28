58d282aaa744ab6b2724a19c6d961aeae3c3d2de
givanse <silvag777@gmail.com>
2013-07-27 19:06:56 -0500

    all the current unit tests pass now

--- test/game/GameServiceTest.java
+++ test/game/GameServiceTest.java
@@ -1,11 +1,17 @@
 package game;
 
+import org.junit.Test;
+
 /**
  *
  * @author givanse
  */
 public class GameServiceTest {
 
-    
+    @Test
+    public void gameServiceTest() {
+        /* TODO: add tests for this class */
+        
+    } 
     
 }
