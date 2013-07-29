949059049289df742156806de5766e850675da4d
givanse <silvag777@gmail.com>
2013-07-29 13:26:15 -0500

    rename Utilery.java

--- test/game/UtilityTest.java
+++ test/game/UtilityTest.java
@@ -7,19 +7,19 @@ import static org.junit.Assert.*;
  *
  * @author givanse
  */
-public class UtileryTest {
+public class UtilityTest {
     
     @Test
     public void shuffleArrayTest() {
         /* empty array */
         int arr[] = new int[0];
-        int actuals[] = Utilery.shuffleArray(arr);
+        int actuals[] = Utility.shuffleArray(arr);
         int expecteds[] = new int[0];
         assertArrayEquals(expecteds, actuals);
         
         /* size 1 */
         arr = new int[]{86};
-        actuals = Utilery.shuffleArray(arr);
+        actuals = Utility.shuffleArray(arr);
         expecteds = new int[]{86};
         assertEquals(1, actuals.length);
         assertEquals(86, actuals[0]);
@@ -28,7 +28,7 @@ public class UtileryTest {
         /* size 2 */
         arr = new int[]{77, 88};
         for(int i = 0; i < 100; i++) {
-            actuals = Utilery.shuffleArray(arr);
+            actuals = Utility.shuffleArray(arr);
             assertEquals(2, actuals.length);
             if(actuals[0] == 88)
                 assertEquals(77, actuals[1]);
@@ -43,7 +43,7 @@ public class UtileryTest {
         /* size 5 */
         arr = new int[]{55, 66, 77, 88, 99};
         for(int i = 0; i < 100; i++) {
-            actuals = Utilery.shuffleArray(arr);
+            actuals = Utility.shuffleArray(arr);
             assertEquals(5, actuals.length);
             /* verify that all the numbers are present */
             for(Integer initNum : arr) {
@@ -67,39 +67,39 @@ public class UtileryTest {
     public void getTwoConsecutiveNumbers() {
         int shuffledArray[] = new int[0];
         try {
-            Utilery.getTwoConsecutiveNumbers(shuffledArray);
+            Utility.getTwoConsecutiveNumbers(shuffledArray);
             fail("Should throw IndexOutOfBoundsException.");
         } catch(IndexOutOfBoundsException ex) {}
         
         shuffledArray = new int[]{1000};
         try {
-            Utilery.getTwoConsecutiveNumbers(shuffledArray);
+            Utility.getTwoConsecutiveNumbers(shuffledArray);
             fail("Should throw Error.");
         } catch(Error err) {}
         
         shuffledArray = new int[]{0, 1};
-        int actuals[] = Utilery.getTwoConsecutiveNumbers(shuffledArray);
+        int actuals[] = Utility.getTwoConsecutiveNumbers(shuffledArray);
         int expectedC1 = 0;
         int expectedC2 = 1;
         assertEquals(expectedC1, actuals[0]);
         assertEquals(expectedC2, actuals[1]);
         
         shuffledArray = new int[]{1, 0};
-        actuals = Utilery.getTwoConsecutiveNumbers(shuffledArray);
+        actuals = Utility.getTwoConsecutiveNumbers(shuffledArray);
         expectedC1 = 1;
         expectedC2 = 0;
         assertEquals(expectedC1, actuals[0]);
         assertEquals(expectedC2, actuals[1]);
         
         shuffledArray = new int[]{4, 5, 6};
-        actuals = Utilery.getTwoConsecutiveNumbers(shuffledArray);
+        actuals = Utility.getTwoConsecutiveNumbers(shuffledArray);
         expectedC1 = 4;
         expectedC2 = 5;
         assertEquals(expectedC1, actuals[0]);
         assertEquals(expectedC2, actuals[1]);
         
         shuffledArray = new int[]{6, 4, 5};
-        actuals = Utilery.getTwoConsecutiveNumbers(shuffledArray);
+        actuals = Utility.getTwoConsecutiveNumbers(shuffledArray);
         expectedC1 = 6;
         expectedC2 = 5;
         assertEquals(expectedC1, actuals[0]);
@@ -108,23 +108,23 @@ public class UtileryTest {
     
     @Test
     public void convertPixelCoordsToSquareCoordsTest() {
-        int actuals[] = Utilery.convertPixelCoordsToSquareCoords(23, 22);
+        int actuals[] = Utility.convertPixelCoordsToSquareCoords(23, 22);
         int expecteds[] = new int[]{0, 0};
         assertArrayEquals(expecteds, actuals);
         
-        actuals = Utilery.convertPixelCoordsToSquareCoords(20, 50);
+        actuals = Utility.convertPixelCoordsToSquareCoords(20, 50);
         expecteds = new int[]{0, 1};
         assertArrayEquals(expecteds, actuals);
         
-        actuals = Utilery.convertPixelCoordsToSquareCoords(50, 18);
+        actuals = Utility.convertPixelCoordsToSquareCoords(50, 18);
         expecteds = new int[]{1, 0};
         assertArrayEquals(expecteds, actuals);
         
-        actuals = Utilery.convertPixelCoordsToSquareCoords(52, 49);
+        actuals = Utility.convertPixelCoordsToSquareCoords(52, 49);
         expecteds = new int[]{1, 1};
         assertArrayEquals(expecteds, actuals);
         
-        actuals = Utilery.convertPixelCoordsToSquareCoords(85, 179);
+        actuals = Utility.convertPixelCoordsToSquareCoords(85, 179);
         expecteds = new int[]{2, 5};
         assertArrayEquals(expecteds, actuals);
         
